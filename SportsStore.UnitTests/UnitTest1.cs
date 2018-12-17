ee6e7d4e770964ba7a4c0d30f406bdc0c4ac0dce
Marcin Kass <marcin.kass10@gmail.com>
2018-12-17 20:35:57 +0100

    WIP: added test but some package are old version and the project have errors

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -1,11 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Controllers;
 
 namespace SportsStore.UnitTests {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        public void TestMethod1() {
+        public void Can_Paginate() {
+            Mock<IProductRepository> mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[]
+            {
+                new Product {ProductID = 1, Name = "p1"},
+                new Product {ProductID = 2, Name = "p2"},
+                new Product {ProductID = 3, Name = "p3"},
+                new Product {ProductID = 4, Name = "p4"},
+                new Product {ProductID = 5, Name = "p5"},
+            });
+
+            ProductController controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            IEnumerable<Product> result = (IEnumerable<Product>) controller.List(2).Model;
+
+            Product[] prodArray = result.ToArray();
+            Assert.IsTrue(prodArray.Length == 2);
+            Assert.AreEqual(prodArray[0].Name, "p4");
+            Assert.AreEqual(prodArray[1].Name, "p5");
+
         }
     }
 }
