7c3dae6e4759ef8a2881da94b745c655f1d01cb8
Marcin Kass <marcin.kass10@gmail.com>
2018-12-18 13:58:55 +0100

    modified routing

--- SportsStore.WebUI/App_Start/RouteConfig.cs
+++ SportsStore.WebUI/App_Start/RouteConfig.cs
@@ -5,24 +5,37 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace SportsStore.WebUI
-{
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
+namespace SportsStore.WebUI {
+    public class RouteConfig {
+        public static void RegisterRoutes(RouteCollection routes) {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null,
-                url: "Strona{page}",
-                defaults: new {Controller = "Product", Action = "List"});
+            routes.MapRoute(null, "",
+                new {
+                    controller = "Product",
+                    action = "List",
+                    category = (string) null,
+                    page = 1
+                });
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Product", action = "List", id = UrlParameter.Optional }
+                null,
+                "Strona{page}",
+                new { controller = "Product", action = "List", category = (string) null },
+                new { page = @"\d+" });
+
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Product", action = "List", page = 1 }
             );
+
+            routes.MapRoute(null,
+                "{category}/Strona{page}",
+                new { controller = "Product", action = "List" },
+                new { page = @"\d+" }
+            );
+
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
