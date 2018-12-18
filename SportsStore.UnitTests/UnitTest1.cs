d1e34702616c6c371457bfc77dd06934e5187a70
Marcin Kass <marcin.kass10@gmail.com>
2018-12-18 13:33:47 +0100

    fixed test after modified List - Class

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -29,7 +29,7 @@ namespace SportsStore.UnitTests {
             controller.PageSize = 3;
 
             //IEnumerable<Product> result = (IEnumerable<Product>) controller.List(2).Model;
-            ProductListViewModel result = (ProductListViewModel) controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel) controller.List(null, 2).Model;
 
             Product[] prodArray = result.Products.ToArray();
             Assert.IsTrue(prodArray.Length == 2);
@@ -73,7 +73,7 @@ namespace SportsStore.UnitTests {
             ProductController controller = new ProductController(mock.Object);
             controller.PageSize = 3;
 
-            ProductListViewModel result = (ProductListViewModel) controller.List(2).Model;
+            ProductListViewModel result = (ProductListViewModel) controller.List(null, 2).Model;
 
             PagingInfo pageInfo = result.PagingInfo;
             Assert.AreEqual(pageInfo.CurrentPage, 2);
