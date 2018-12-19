cbabd963e4a8706a01d151f95d86efc91ee32a74
Marcin Kass <marcin.kass10@gmail.com>
2018-12-19 13:52:17 +0100

    added two class for buying product and cart where the products are kept

---
+++ SportsStore.Domain/Entities/CartLine.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Domain.Entities {
+    public class CartLine {
+        public Product Product { get; set; }
+        public int Quality { get; set; }
+
+    }
+}
