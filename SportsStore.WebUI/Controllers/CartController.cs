958bfcda7d225e0f1b049e742384cb9ddf5f3325
Marcin Kass <marcin.kass10@gmail.com>
2018-12-20 20:43:37 +0100

    added partial view for navigation to cart where products are kept

--- SportsStore.WebUI/Controllers/CartController.cs
+++ SportsStore.WebUI/Controllers/CartController.cs
@@ -66,6 +66,10 @@ namespace SportsStore.WebUI.Controllers {
             return cart;
         }
 
+        public PartialViewResult Summary(Cart cart)
+        {
+            return PartialView(cart);
+        }
 
     }
 }
