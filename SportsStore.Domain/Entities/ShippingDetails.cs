a4d8239691d6020664095a216eaeddbc8b3bafea
Marcin Kass <marcin.kass10@gmail.com>
2018-12-21 18:08:11 +0100

    some refactoring for adres data and modified shippingDetails - added Displa "Name"

--- SportsStore.Domain/Entities/ShippingDetails.cs
+++ SportsStore.Domain/Entities/ShippingDetails.cs
@@ -10,16 +10,22 @@ namespace SportsStore.Domain.Entities {
         [Required(ErrorMessage = "Proszę podać nazwisko")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Proszę podać adres wysyłki")]
+        [Display(Name = "Wiersz 1")]
         public string Line1 { get; set; }
+        [Display(Name = "Wiersz 2")]
         public string Line2 { get; set; }
+        [Display(Name = "Wiersz 3")]
         public string Line3 { get; set; }
         [Required(ErrorMessage = "Proszę podać nazwę miasta")]
+        [Display(Name = "Miasto")]
         public string City { get; set; }
         [Required(ErrorMessage = "Proszę podać nazwę województwa")]
+        [Display(Name = "Województwo")]
         public string State { get; set; }
-
+        [Display(Name = "Kod pocztowy")]
         public string Zip { get; set; }
         [Required(ErrorMessage = "Proszę podać nazwę kraju")]
+        [Display(Name = "Kraj")]
         public string Country { get; set; }
 
         public bool GiftWrap { get; set; }
