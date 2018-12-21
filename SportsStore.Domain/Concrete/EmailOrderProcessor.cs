78f4b8be9431dedae05cee5f542378d99fb8cc40
Marcin Kass <marcin.kass10@gmail.com>
2018-12-21 19:16:10 +0100

    added smptp sample for sending info about order placed

---
+++ SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.Domain.Concrete {
+    public class EmailSettings {
+        public string MailToAdress = "zamowienia@przyklad.pl";
+        public string MailFromAdress = "sklepsportowy@przyklad.pl";
+        public bool UseSsl = true;
+        public string Username = "UżytkownikSmtp";
+        public string Password = "HassłoSmtp";
+        public string ServerName = "smtp.przyklad.pl";
+        public int ServerPort = 587;
+        public bool WriteAsFile = false;
+        public string FileLocation = @"c:\sports_store_emails";
+    }
+
+    public class EmailOrderProcessor : IOrderProcessor {
+        private EmailSettings emailSettings;
+
+        public EmailOrderProcessor(EmailSettings settings) {
+            emailSettings = settings;
+        }
+
+        public void ProcessOrder(Cart cart, ShippingDetails shippingDetails) {
+            using (var smtpClient = new SmtpClient()) {
+                smtpClient.EnableSsl = emailSettings.UseSsl;
+                smtpClient.Host = emailSettings.ServerName;
+                smtpClient.Port = emailSettings.ServerPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(emailSettings.Username, emailSettings.Password);
+                if (emailSettings.WriteAsFile) {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+                StringBuilder body = new StringBuilder().AppendLine("Nowe Zamówienie").AppendLine("---").AppendLine("Produkty:");
+
+                foreach (var line in cart.Lines) {
+                    var subtotal = line.Product.Price * line.Quantity;
+                    body.AppendFormat("{0} * {1} (Wartość: {2:C}", line.Quantity, line.Product.Name, subtotal);
+                }
+
+                body.AppendFormat("Wartość całkowita: {0:C}", cart.ComputeTotalValue())
+                    .AppendLine("---")
+                    .AppendLine("Wysyłka dla:")
+                    .AppendLine(shippingDetails.Name)
+                    .AppendLine(shippingDetails.Line1)
+                    .AppendLine(shippingDetails.Line2 ?? "")
+                    .AppendLine(shippingDetails.Line3 ?? "")
+                    .AppendLine(shippingDetails.City)
+                    .AppendLine(shippingDetails.State ?? "")
+                    .AppendLine(shippingDetails.Country)
+                    .AppendLine(shippingDetails.Zip)
+                    .AppendLine("---")
+                    .AppendFormat("Pakowanie prezentu: {0}", shippingDetails.GiftWrap ? "Tak" : "Nie");
+
+                MailMessage mailMessage = new MailMessage(
+                    emailSettings.MailFromAdress,
+                    emailSettings.MailToAdress,
+                    "Otrzymano nowe zamówienie",
+                    body.ToString());
+                if (emailSettings.WriteAsFile) {
+                    mailMessage.BodyEncoding = Encoding.ASCII;
+                }
+                smtpClient.Send(mailMessage);
+            }
+        }
+    }
+}
