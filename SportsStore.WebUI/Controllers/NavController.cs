c6cc08f1a6f014013cd7aa98753dfe4c63eb199e
Marcin Kass <marcin.kass10@gmail.com>
2019-01-09 15:11:00 +0100

    added more details for mobile devices view

--- SportsStore.WebUI/Controllers/NavController.cs
+++ SportsStore.WebUI/Controllers/NavController.cs
@@ -16,12 +16,13 @@ namespace SportsStore.WebUI.Controllers {
             repository = repo;
         }
         // GET: Nav
-        public PartialViewResult Menu(string category = null)
+        public PartialViewResult Menu(string category = null, bool horizontalLayout = false)
         {
             ViewBag.SelectedCategory = category;
             IEnumerable<string> categories = repository.Products.Select(x => x.Category).Distinct().OrderBy(x => x);
 
-            return PartialView(categories);
+            string viewName = horizontalLayout ? "MenuHorizontal" : "Menu";
+            return PartialView(viewName, categories);
         }
     }
 }
