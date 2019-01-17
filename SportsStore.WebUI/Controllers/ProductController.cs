5b70da28d10f602c04435c4fe52f63dc00030d5a
Marcin Kass <marcin.kass10@gmail.com>
2019-01-17 02:18:31 +0100

    fixed last bug - Home button

--- SportsStore.WebUI/Controllers/ProductController.cs
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -4,18 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers {
+
     public class ProductController : Controller {
         private IProductRepository repository;
         public int PageSize = 4;
+
         public ProductController(IProductRepository productRepository) {
             this.repository = productRepository;
         }
-        // GET: Product
+
         public ViewResult List(string category, int page = 1) {
-            ProductListViewModel model = new ProductListViewModel {
+
+            ProductListViewModel viewModel = new ProductListViewModel {
                 Products = repository.Products
                     .Where(p => category == null || p.Category == category)
                     .OrderBy(p => p.ProductID)
@@ -30,7 +34,8 @@ namespace SportsStore.WebUI.Controllers {
                 },
                 CurrentCategory = category
             };
-            return View(model);
+            return View(viewModel);
         }
+
     }
 }
