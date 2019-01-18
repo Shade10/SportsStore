37b8a48ebf22000289a596c2d55c5116bbb83ae7
Marcin Kass <marcin.kass10@gmail.com>
2019-01-18 20:39:05 +0100

    added syntax error for products managment

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -10,12 +10,21 @@ namespace SportsStore.Domain.Entities {
     public class Product {
         [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać nazwę produktu.")]
         [Display(Name = "Nazwa")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Proszę podać Opis.")]
         [DataType(DataType.MultilineText), Display(Name = "Opis")]
         public string Description { get; set; }
+
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Proszę podać dodatnią cenę.")]
         [Display(Name = "Cena")]
         public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Proszę określić kategorię.")]
         [Display(Name = "Kategoria")]
         public string Category { get; set; }
 
