e5ca4205bd105d37e658427d161dd4c002c9cf44
Marcin Kass <marcin.kass10@gmail.com>
2019-01-19 10:58:16 +0100

    not saved code line fixed

--- SportsStore.WebUI/Controllers/AccontController.cs
+++ SportsStore.WebUI/Controllers/AccontController.cs
@@ -4,17 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Models;
 
 namespace SportsStore.WebUI.Controllers {
     public class AccontController : Controller {
-        private IAuthProvider authProvider;
+        IAuthProvider authProvider;
 
         public AccontController(IAuthProvider auth) {
             authProvider = auth;
         }
         // GET: Accont
-        public ActionResult Index() {
+        public ViewResult Login() {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl) {
+            if (ModelState.IsValid) {
+                if (authProvider.Authenticate(model.UseName, model.Password)) {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                } else {
+                    ModelState.AddModelError("", "Nieprawidłowa nazwa użytkownika lub haslo");
+                }
+
+                return View();
+            } else {
+                return View();
+            }
         }
     }
 }
