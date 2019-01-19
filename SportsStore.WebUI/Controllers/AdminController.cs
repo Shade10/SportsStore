809104fe44adb247e71294d61aceeb282fd53fac
Marcin Kass <marcin.kass10@gmail.com>
2019-01-19 13:20:35 +0100

    added image feature for editing and view image

--- SportsStore.WebUI/Controllers/AdminController.cs
+++ SportsStore.WebUI/Controllers/AdminController.cs
@@ -6,55 +6,48 @@ using System.Web.Mvc;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Entities;
 
-namespace SportsStore.WebUI.Controllers
-{
+namespace SportsStore.WebUI.Controllers {
     [Authorize]
-    public class AdminController : Controller
-    {
+    public class AdminController : Controller {
         private IProductRepository repository;
 
-        public AdminController(IProductRepository repo)
-        {
+        public AdminController(IProductRepository repo) {
             repository = repo;
         }
         // GET: Admin
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             return View(repository.Products);
         }
 
-        public ViewResult Edit(int productId)
-        {
+        public ViewResult Edit(int productId) {
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
             return View(product);
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Edit(Product product, HttpPostedFileBase image = null) {
+            if (ModelState.IsValid) {
+                if (image != null) {
+                    product.ImageMimeType = image.ContentType;
+                    product.ImageData = new byte[image.ContentLength];
+                    image.InputStream.Read(product.ImageData, 0, image.ContentLength);
+                }
                 repository.SaveProduct(product);
                 TempData["message"] = string.Format("Zapisano {0}", product.Name);
                 return RedirectToAction("Index");
-            }
-            else
-            {
+            } else {
                 return View(product);
             }
         }
 
-        public ViewResult Create()
-        {
+        public ViewResult Create() {
             return View("Edit", new Product());
         }
 
         [HttpPost]
-        public ActionResult Delete(int productId)
-        {
+        public ActionResult Delete(int productId) {
             Product deletedProduct = repository.DeleteProduct(productId);
-            if (deletedProduct != null)
-            {
+            if (deletedProduct != null) {
                 TempData["message"] = string.Format("Usunięto {0}", deletedProduct.Name);
             }
 
