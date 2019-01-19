288972cd28bde8e942e7af0b727c823d69049e7b
Marcin Kass <marcin.kass10@gmail.com>
2019-01-19 10:57:04 +0100

    added controller for account signUo/signIn

---
+++ SportsStore.WebUI/Controllers/AccontController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.WebUI.Infrastructure.Abstract;
+
+namespace SportsStore.WebUI.Controllers {
+    public class AccontController : Controller {
+        private IAuthProvider authProvider;
+
+        public AccontController(IAuthProvider auth) {
+            authProvider = auth;
+        }
+        // GET: Accont
+        public ActionResult Index() {
+            return View();
+        }
+    }
+}
